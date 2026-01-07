90797b4b2adcb40fc9a907b727a309d46de2f97f
Fabio Souza <souzace@gmail.com>
2026-01-07 10:40:50 -0300

    feat: add benefitService layer

---
+++ services/pix-api-java/src/main/java/com/pix/bip/service/BenefitService.java
@@ -0,0 +1,32 @@
+package com.pix.bip.service;
+
+import com.pix.bip.model.Benefit;
+import com.pix.bip.repository.BenefitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BenefitService {
+
+    @Autowired
+    private BenefitRepository benefitRepository;
+
+    public Benefit createBenefit(Benefit benefit) {
+        return benefitRepository.save(benefit);
+    }
+
+    public List<Benefit> getAllBenefits() {
+        return benefitRepository.findAll();
+    }
+
+    public Optional<Benefit> getBenefitById(Long id) {
+        return benefitRepository.findById(id);
+    }
+
+    public void deleteBenefit(Long id) {
+        benefitRepository.deleteById(id);
+    }
+}
