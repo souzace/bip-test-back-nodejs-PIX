be5892f1a3ca2b86086a2f5e1a1e1faceae161cb
Fabio Souza <souzace@gmail.com>
2026-01-07 10:07:02 -0300

    feat: add Benefit Model Entity

---
+++ services/pix-api-java/src/main/java/com/pix/bip/model/Benefit.java
@@ -0,0 +1,38 @@
+package com.pix.bip;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Benefit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private BigDecimal discountPercentage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(BigDecimal discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+}
