d768bb60d619a42dbb09254eb393003ea0d7fc30
Fabio Souza <souzace@gmail.com>
2026-01-07 23:33:54 -0300

    feat: update db-init script to run new properties

--- scripts/db-init.sql
+++ scripts/db-init.sql
@@ -2,5 +2,7 @@ CREATE TABLE pix_payment (
   id UUID PRIMARY KEY,
   amount NUMERIC,
   status VARCHAR(30),
-  created_at TIMESTAMP
+  created_at TIMESTAMP,
+  sender_pix_key VARCHAR(100),
+  receiver_pix_key VARCHAR(100)
 );
