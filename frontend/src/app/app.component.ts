1fdd89115a820ef6741fef71fa280cb1b4a790b6
Fabio Souza <souzace@gmail.com>
2026-01-10 22:59:21 -0300

    feat: start frontend in angular 17

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>PIX Payment System</h1>',
+  styles: []
+})
+export class AppComponent {
+  title = 'PIX Payment System';
+}
